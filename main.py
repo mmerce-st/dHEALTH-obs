dfae73fe8f0f36743c52c4fb91737d60f3cb767b
Mercè Martín Prats <merce@bigml.com>
2025-10-12 11:00:31 +0200

    Adding title

--- main.py
+++ main.py
@@ -66,6 +66,8 @@ temp_df = conn.read(
 if "student" not in st.session_state:
     st.session_state.student = 0
 
+st.markdown("### Observations")
+
 with st.form("Add observation", clear_on_submit=True):
     col_observation, col_student = st.columns([80, 20])
     with col_observation:
